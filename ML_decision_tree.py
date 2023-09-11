f1fb28ef46fb388df161a4f1f342ac1206c94274
Rohit7777777 <rohitdabola77@gmail.com>
2023-09-11 15:18:47 +0530

    first commit

---
+++ ML_decision_tree.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import sklearn
+from sklearn import tree
+features = [[140,1],[130,1],[150,0],[170,0]]
+labels = [0,0,1,1]
+clf = tree.DecisionTreeClassifier()
+clf=clf.fit(features,labels)
+print(clf.predict([[150,0]]))
+# 
+'''
+
+'''
